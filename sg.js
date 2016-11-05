d54277ded7b76b1a57931f1f109579da92eddafc
joshualemli <joshualemli@gmail.com>
2016-11-05 13:25:16 -0400

    workin

--- sg.js
+++ sg.js
@@ -7,32 +7,51 @@ var SG = (function(){
 
   var canvas;
   var context;
-  var debug;//for printing msg to html dom element
-  
+  var info; // output messages
+  var Make; // object creation module
+
   function resizeWindow() {
     context.canvas.width = canvas.offsetWidth;
     context.canvas.height = canvas.offsetHeight;
-    console.log(context.canvas.height);
   }
-  
-  var gamedata = {
-    
+
+  var objects = {};
+
+  var game = {
+    iteration: 0, // persistent loop counter
+    date: 0, // milliseconds
+    playtime: 0, // milliseconds
+    formatDate: function(){return this.date/1000+'s';},
+    framerate: 0, // last average
+    _framerate: 0, // rolling total
+    _framecount: 0, // frames in `_framerate`
   };
-  var iteration = 0;
-  
-  function gameplay() {
-    iteration += 1;
-    debug.innerHTML = iteration;
+
+  function gameplay(_t) {
+    game.iteration += 1;
+    var dt = _t - game.playtime;
+    game.playtime = _t;
+    game.date += dt;
+    if (game._framecount < 60) {
+      game._framecount += 1;
+      game._framerate += dt;
+    }
+    else {
+      game.framerate = parseInt(game._framerate/game._framecount);
+      game._framecount = 0;
+      game._framerate = 0;
+    }
+    game._framecoun
+    var msg = game.date + 's<br>i: ' + game.iteration + '<br>framerate: ' + game.framerate;
     context.clearRect(0,0,context.canvas.width,context.canvas.height);
-    
     context.fillStyle = '#00FF00';
     context.fillRect(100,100,100,100);
-    
+
     window.requestAnimationFrame(gameplay);
   }
 
   function init() {
-    debug = document.getElementById('debug');
+    info = document.getElementById('info');
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
     resizeWindow();
@@ -40,9 +59,11 @@ var SG = (function(){
   }
 
   return {
-    init:init
+    init:init,
+    resizeWindow:resizeWindow
   };
 
 })();
 
 window.onload = SG.init;
+window.addEventListener('resize',SG.resizeWindow);
