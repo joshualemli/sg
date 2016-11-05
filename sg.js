51c2e472e8455e82d31d658c5d8b979dca6ab559
joshualemli <joshualemli@gmail.com>
2016-11-04 21:22:33 -0400

    basic update, html dom, css, javascript

--- sg.js
+++ sg.js
@@ -1,9 +1,42 @@
 
+//    Sarah's Garden
+//    Joshua A. Lemli
+//    2016
+
 var SG = (function(){
 
+  var canvas;
+  var context;
+  var debug;//for printing msg to html dom element
+  
+  function resizeWindow() {
+    context.canvas.width = canvas.offsetWidth;
+    context.canvas.height = canvas.offsetHeight;
+    console.log(context.canvas.height);
+  }
+  
+  var gamedata = {
+    
+  };
+  var iteration = 0;
+  
+  function gameplay() {
+    iteration += 1;
+    debug.innerHTML = iteration;
+    context.clearRect(0,0,context.canvas.width,context.canvas.height);
+    
+    context.fillStyle = '#00FF00';
+    context.fillRect(100,100,100,100);
+    
+    window.requestAnimationFrame(gameplay);
+  }
+
   function init() {
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
+    debug = document.getElementById('debug');
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+    resizeWindow();
+    window.requestAnimationFrame(gameplay);
   }
 
   return {
